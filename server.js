480b066cc409c989b2134f99c45efb775a692f5c
chrismadden29 <156925050+chrismadden29@users.noreply.github.com>
2026-04-20 19:30:23 -0400

    making it work with mongodb

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const controller = require('./controller/controller');
+const dbcon = require('./DbConnection');
 
 app.use(express.json());
 app.use(express.static('public_html'));
 
 app.get('/api/fibonacci-facts', controller.getAllFacts);
-app.get('/api/fibonacci-facts/:id', controller.get);
-app.post('/api/fibonacci-facts', controller.create);
-app.put('/api/fibonacci-facts/:id', controller.update);
-app.delete('/api/fibonacci-facts/:id', controller.delete);
+app.get('/api/fibonacci-facts/:id', controller.getFact);
+app.post('/api/fibonacci-facts', controller.createFact);
+app.put('/api/fibonacci-facts/:id', controller.updateFact);
+app.delete('/api/fibonacci-facts/:id', controller.delFact);
 
-app.listen(4000, function(){
+async function startServer(){
+    await dbcon.connect();
+    app.listen(4000, function(){
     console.log('Server running on localhost:4000');
-});
+    });
+}
+
+startServer();
+
